29dca6f859c595db8d99b4b6408b88dae31a4878
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2023-02-04 10:42:54 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// const url = `https://dog.ceo/api/breeds/image/random`;
+
+// fetch(url)
+//     .then(response => {
+//         console.log(response);
+//         return response.json();
+//     })
+//     .then(data => {
+//         console.log(data.message);
+//         const img = document.createElement("img");
+//         document.body.append(img);
+//         img.src = data.message;
+
+
+//     })
+//     .catch(error => console.log(error));
+
+
+
+//////De olika stegen:
+
+// const fetchPromise = fetch(url);
+// console.log('fetch', fetchPromise);
+
+
+// const jsonPromise = fetchPromise.then(
+//     response => {
+//         console.log(response);
+//         return response.json()
+//     });
+// console.log('json', jsonPromise);
+
+// jsonPromise.then(data => console.log(data));
+
+// console.log('Sist i koden');
+
+
+
+
